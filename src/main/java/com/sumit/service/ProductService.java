451799eab805f60a12697e6af3cfa16b70fb182d
sumitbasu116@gmail.com <sumitbasu116@gmail.com>
2025-01-04 17:14:29 +0530

    void and verify

--- src/main/java/com/sumit/service/ProductService.java
+++ src/main/java/com/sumit/service/ProductService.java
@@ -13,44 +13,46 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class ProductService {
-	
+
 	private final ProductRepository productRepository;
-	
-	//Constructor Injection (preferred approach in modern Spring applications)
+
+	// Constructor Injection (preferred approach in modern Spring applications)
 	public ProductService(ProductRepository productRepository) {
-		this.productRepository=productRepository;
+		this.productRepository = productRepository;
 	}
-	
+
 	public Product addProduct(Product product) {
-		
+
 		Product savedProduct = productRepository.save(product);
 		log.info("Product saved successfully in DB");
 		return savedProduct;
 	}
-	
-	// Void method to demonstrate mocking of voids
-	public void updateProductStock(long productId,int quantity) {
-		Optional<Product> productById = productRepository.findById(productId);
-		if(productById.isPresent()) {
-			Product product = productById.get();
-			int newStockCount=product.getStock()+quantity;
-			product.setStock(newStockCount);
-			Product saveProduct = productRepository.save(product);
-			log.info("New stock level for product {}: {}", productId, saveProduct.getStock());
-		}
-		else {
-            log.warn("Product with ID: {} not found", productId);
-        }
-	}
 
 	public Product getProductByName(String productName) {
 		Optional<Product> productByName = productRepository.findByName(productName);
-		if(productByName.isPresent()) {
+		if (productByName.isPresent()) {
 			return productByName.get();
-		}
-		else {
+		} else {
 			log.error("Product with Name: {} not found", productName);
 			throw new ProductNotFoundException(productName);
+		}
+	}
+
+	// Void method to demonstrate mocking of voids
+	public void deleteProductById(long productId) {
+		productRepository.deleteById(productId);
+	}
+	// Void method to demonstrate mocking of voids
+	public void updateProductStock(long productId, int quantity) {
+		Optional<Product> productById = productRepository.findById(productId);
+		if (productById.isPresent()) {
+			Product product = productById.get();
+			int newStockCount = product.getStock() + quantity;
+			product.setStock(newStockCount);
+			Product saveProduct = productRepository.save(product);
+			log.info("New stock level for product {}: {}", productId, saveProduct.getStock());
+		} else {
+			log.warn("Product with ID: {} not found", productId);
 		}
 	}
 }
