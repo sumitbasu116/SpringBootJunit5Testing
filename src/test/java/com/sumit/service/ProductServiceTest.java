c1edbc58c1b2d051123557008815c49491ad73d8
sumitbasu116@gmail.com <sumitbasu116@gmail.com>
2025-01-04 19:19:22 +0530

    test method throwing exceptions

--- src/test/java/com/sumit/service/ProductServiceTest.java
+++ src/test/java/com/sumit/service/ProductServiceTest.java
@@ -1,5 +1,12 @@
 package com.sumit.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.times;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -45,5 +52,56 @@ public class ProductServiceTest {
 		//4.  Assertions logic
 		Assertions.assertEquals(product.getId(), addedProduct.getId());
 		Assertions.assertTrue(product.getId()==1);
+	}
+	
+	@Test
+	public void deleteProductToDeleteFromDB() {
+		log.info("Delete product testing...");
+		//1.Data
+		long deleteId=1;
+		
+		//2. Mocking call logic if any
+		Mockito.doNothing().when(productRepository).deleteById(deleteId);
+		
+		//3. Actual test method call
+		productService.deleteProductById(deleteId);
+		
+		//4.  Assertions logic
+		//verify the deleteById method of productRepository class is called one time.
+		Mockito.verify(productRepository, times(1)).deleteById(deleteId);
+	}
+	
+	@Test
+	//private method can be tested by using Java reflection feature.
+	public void validateProductValidNamePrivateTest() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		
+		Method validateProductNameMethod = 
+				productService.getClass().getDeclaredMethod("validateProductName",String.class);
+		validateProductNameMethod.setAccessible(true);
+		boolean result =(boolean) validateProductNameMethod.invoke(productService,"product-abc");
+		Assertions.assertTrue(result);
+	}
+	@Test
+	//private method can be tested by using Java reflection feature.
+	public void validateProductInValidNamePrivateTest() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		
+		Method validateProductNameMethod = 
+				productService.getClass().getDeclaredMethod("validateProductName",String.class);
+		validateProductNameMethod.setAccessible(true);
+		boolean result =(boolean) validateProductNameMethod.invoke(productService,"");
+		Assertions.assertFalse(result);
+	}
+	
+	@Test
+	public void addProductShouldThrowExceptionForInvalidProductName(){
+		log.info("Add product testing...");
+		
+		Product product = new Product(1, "", 300, 2);
+		
+		RuntimeException runtimeException = assertThrows(RuntimeException.class, ()->{
+			productService.addProduct(product);
+		});
+		//check the specific message
+		assertEquals("Invalid Product Name", runtimeException.getMessage());
 	}
 }
